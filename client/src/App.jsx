3b8d0dd76926c0546224ee396bd86c3f0a3c5675
WAJAHAT-ANSARI2 <m.wajahatansari2004@gmail.com>
2025-08-23 11:09:04 +0000

    sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,20 +8,24 @@ import Connections from './Pages/Connections'
 import Discover from './Pages/Discover'
 import Profile from './Pages/Profile'
 import CreatePost from './Pages/CreatePost'
+import { useUser } from "@clerk/clerk-react";
+import  Layout  from "./Pages/Layout";
 
 const App = () => {
+  const {user} = useUser()
   return(
     <>
       <Routes>
-        <Route path="/" element={<Login/>}>
+        <Route path="/" element={!user ? <Login/> : <Layout/>}>
           <Route index element={<Feed/>} />
-          <Route path="messages" element={<Messages/>}></Route>
-          <Route path="messages/:userId" element={<ChatBox/>}></Route>
-          <Route path="connections" element={<Connections/>}></Route>
-          <Route path="messages/:userId" element={<ChatBox/>}></Route>
-          <Route path="discover" element={<Discover/>}></Route>
-          <Route path="profile/:profileId" element={<Profile/>}></Route>
-          <Route path="create-post" element={<CreatePost/>}></Route>
+          <Route path="messages" element={<Messages/>}/>
+          <Route path="messages/:userId" element={<ChatBox/>}/>
+          <Route path="connections" element={<Connections/>}/>
+          <Route path="messages/:userId" element={<ChatBox/>}/>
+          <Route path="discover" element={<Discover/>}/>
+          <Route path="profile" element={<Profile/>}/>
+          <Route path="profile/:profileId" element={<Profile/>}/>
+          <Route path="create-post" element={<CreatePost/>}/>
           
         </Route>
       </Routes>
